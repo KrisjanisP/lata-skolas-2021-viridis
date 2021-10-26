6a686ec64c2a06ae6d42f268b73f517e3d91bc66
krisjanisp <krisjanispetrucena@gmail.com>
2021-10-26 20:19:16 +0300

    get rid of python

--- front/src/index.js
+++ front/src/index.js
@@ -2,6 +2,27 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./style.css";
 
-import { InitMap } from './openlayers.js';
+import { initMap, handleMapClicks, getSelectedFields } from './openlayers.js';
 
-InitMap();
+initMap();
+handleMapClicks();
+
+window.submitSelectedFields = function(){
+  console.log("hello");
+  let selected = getSelectedFields()
+  console.log(JSON.stringify(selected));
+  fetch('http://127.0.0.1:5000/tks93tiles', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(selected),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
--- front/src/openlayers.js
+++ front/src/openlayers.js
@@ -1,71 +1,92 @@
-
 import "ol/ol.css";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import VectorSource from "ol/source/Vector";
-import VectorImageLayer from 'ol/layer/VectorImage';
+import VectorImageLayer from "ol/layer/VectorImage";
 import GeoJSON from "ol/format/GeoJSON";
-import Style from 'ol/style/Style';
-import Fill from 'ol/style/Fill';
-import Stroke from 'ol/style/Stroke';
-import {
-  defaults as defaultInteractions,
-} from 'ol/interaction';
+import Style from "ol/style/Style";
+import Fill from "ol/style/Fill";
+import Stroke from "ol/style/Stroke";
+import { defaults as defaultInteractions } from "ol/interaction";
 
-export function InitMap(){
+let map;
 
-    const raster = new TileLayer({
-        source: new OSM(),
-      });
+export function initMap() {
+  const raster = new TileLayer({
+    source: new OSM(),
+  });
+
+  const vector = new VectorImageLayer({
+    source: new VectorSource({
+      url: "http://127.0.0.1:5000/tks93tiles",
+      format: new GeoJSON({ dataProjection: "EPSG:3857" }),
+    }),
+  });
+
+  map = new Map({
+    layers: [raster, vector],
+    target: "map",
+    view: new View({
+      // center on Latvia. these coordinates can be found by map.getView().getCenter()
+      center: [2734027.654715377, 7718479.091241797],
+      zoom: 9,
+    }),
+    interactions: defaultInteractions({ doubleClickZoom: false }),
+  });
+}
+
+let selected = [];
+
+export function getSelectedFields(){
+  let selectedFields = selected.map((data) => data.id_);
+  return selectedFields;
+}
+
+function toggleButtonState() {
+    if (selected.length)
+      document
+        .getElementById("fields-submit-btn")
+        .classList.remove("disabled");
+    else
+      document.getElementById("fields-submit-btn").classList.add("disabled");
+
+}
+
+function updateSelectedFieldsList() {
+    let selectedFields = getSelectedFields();
+    document.getElementById("selected-fields").innerHTML =
+      selectedFields.join(", ");
+}
+
+export function handleMapClicks() {
+  // https://openlayers.org/en/latest/examples/select-multiple-features.html
+
+  const highlightStyle = new Style({
+    fill: new Fill({
+      color: "rgb(120,160,71,0.8)",
+    }),
+    stroke: new Stroke({
+      color: "#3399CC",
+      width: 3,
+    }),
+  });
+
+  map.on("click", function (e) {
+    map.forEachFeatureAtPixel(e.pixel, function (f) {
+
+      const selIndex = selected.indexOf(f);
+      if (selIndex < 0) {
+        selected.push(f);
+        f.setStyle(highlightStyle);
+      } else {
+        selected.splice(selIndex, 1);
+        f.setStyle(undefined);
+      }
       
-      const vector = new VectorImageLayer({
-        source: new VectorSource({
-          url: "http://127.0.0.1:5000/tks93tiles",
-          format: new GeoJSON({dataProjection: 'EPSG:3857'}),
-        }),
-      });
+      toggleButtonState()
+      updateSelectedFieldsList()
       
-      const map = new Map({
-        layers: [raster, vector],
-        target: "map",
-        view: new View({
-          // center on Latvia. these coordinates can be found by map.getView().getCenter()
-          center: [2734027.654715377, 7718479.091241797],
-          zoom: 9,
-        }),
-        interactions: defaultInteractions({doubleClickZoom:false})
-      });
-      
-      // https://openlayers.org/en/latest/examples/select-multiple-features.html
-      
-      const highlightStyle = new Style({
-        fill: new Fill({
-          color: 'rgb(120,160,71,0.8)',
-        }),
-        stroke: new Stroke({
-          color: '#3399CC',
-          width: 3,
-        }),
-      });
-      
-      let selected = [];
-      
-      map.on('click', function (e) {
-        map.forEachFeatureAtPixel(e.pixel, function (f) {
-          const selIndex = selected.indexOf(f);
-          if (selIndex < 0) {
-            selected.push(f);
-            f.setStyle(highlightStyle);
-          } else {
-            selected.splice(selIndex, 1);
-            f.setStyle(undefined);
-          }
-          if(selected.length) document.getElementById('fields-submit-btn').classList.remove('disabled')
-          else document.getElementById('fields-submit-btn').classList.add('disabled')
-          let selectedFields = selected.map((data)=>data.id_)
-          console.log(selectedFields)
-          document.getElementById('selected-fields').innerHTML=selectedFields.join(", ")
-        });
-      });
+    });
+  });
 }
