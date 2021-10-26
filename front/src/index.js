0c39c49de904ad7063d4c7c59c8c3e9f510cb996
krisjanisp <krisjanispetrucena@gmail.com>
2021-10-26 11:19:38 +0300

    clean up js

--- front/src/index.js
+++ front/src/index.js
@@ -4,42 +4,16 @@ import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
+import VectorImageLayer from 'ol/layer/VectorImage';
 import GeoJSON from "ol/format/GeoJSON";
 import Select from "ol/interaction/Select";
-import Feature from "ol/Feature";
-import Point from "ol/geom/Point";
-import { Icon, Style } from "ol/style";
 import { click } from "ol/events/condition";
-import { fromLonLat } from "ol/proj";
-
-const geojsonObject = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      'type': 'Feature',
-      "id":"hello1",
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [2924375.3909816, 7507759.76255841],
-      },
-    }
-  ],
-};
-
-const vectorSource = new VectorSource({
-  features: new GeoJSON().readFeatures(geojsonObject),
-});
-
-const vectorLayer = new VectorLayer({
-  source: vectorSource,
-});
 
 const raster = new TileLayer({
   source: new OSM(),
 });
 
-
-const vector = new VectorLayer({
+const vector = new VectorImageLayer({
   source: new VectorSource({
     url: "http://127.0.0.1:5000/",
     format: new GeoJSON({dataProjection: 'EPSG:3857'}),
@@ -52,8 +26,7 @@ const map = new Map({
   view: new View({
     // center on Latvia. these coordinates can be found by map.getView().getCenter()
     center: [2734027.654715377, 7718479.091241797],
-    //zoom: 8,
-    zoom: 3,
+    zoom: 9,
   }),
 });
 
